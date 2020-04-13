1d6cbee049f7118b7198454dde17a894971c4bcd
serano.colameo <serano@colameo.ch>
2020-04-13 23:03:01 +0200

    refactored

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-npm install -g ajv-cli
-ajv validate -s ./src/JDLApplicationOptions.schema.json -d ./src/JDLApplicationOptions.json
-ajv validate -s ./src/JDLDeploymentOptions.schema.json -d ./src/JDLDeploymentOptions.json
+files=$(find src -name *.json ! -path "*.schema.*" | awk -F/ '{ print $NF }' | cut -d '.' -f 1)
+
+for f in $files
+do
+	echo "Validating $f.json"
+	ajv validate -s ./src/$f.schema.json -d ./src/$f.json
+done
