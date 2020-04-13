a89742a70723eee0027ddf719f917b1da7093644
serano.colameo <serano@colameo.ch>
2020-04-13 22:01:10 +0200

    retry

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+npm install -g ajv-cli
 ajv validate -s ./src/JDLApplicationOptions.schema.json -d ./src/JDLApplicationOptions.json
 ajv validate -s ./src/JDLDeploymentOptions.schema.json -d ./src/JDLDeploymentOptions.json
